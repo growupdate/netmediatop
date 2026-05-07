865adcce926e558ec9ea227554b127878b7354c9
tri <netmediaku99@gmail.com>
2026-05-07 22:59:02 +0700

    initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
 export default defineConfig({
-  site: 'https://netmediatop.com',
-  // base: '/netmediatop', <-- HAPUS baris ini atau beri tanda // di depannya
+  site: 'https://growupdate.com',
+  // base: '/growupdate', <-- HAPUS baris ini atau beri tanda // di depannya
   integrations: [sitemap(), icon(), mdx()],
   // ... kode lainnya
   i18n: {
