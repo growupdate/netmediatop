6f9af764eb82a12bace95d722e085dc45184d2c2
tri <netmediaku99@gmail.com>
2026-09-09 21:24:26 +0700

    chore: update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+  output: 'static', // <-- Tambahkan baris ini
   adapter: cloudflare(),
 });
