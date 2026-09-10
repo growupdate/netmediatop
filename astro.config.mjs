938814ccda8c8b2975013ae0576c1f77a1adf3a1
tri <netmediaku99@gmail.com>
2026-09-10 11:00:43 +0700

    fix: add astro-icon integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 export default defineConfig({
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
   },
