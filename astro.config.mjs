2112036135009d29ecf2daf07ada8b3aa87eec44
tri <netmediaku99@gmail.com>
2026-09-10 11:02:42 +0700

    fix: add astro-icon integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
 export default defineConfig({
+  site: 'https://netmediatop.com',
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
